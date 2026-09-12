60b0301d0235a7195ac94d6275948f7fb8df4dbf
Erkki Nurmi <183535537+eki-77@users.noreply.github.com>
2026-09-12 23:25:46 +0300

    add src folder and fft.py

--- dokumentaatio/viikkoraportti_2.md
+++ dokumentaatio/viikkoraportti_2.md
@@ -2,7 +2,12 @@ Viikko 2:
 
 Tällä viikolla käytin aivan liian vähän aikaa tähän kurssiin. Opiskelin poetry-kuviota ja testaamista ylipäätään. Tämä on minulle uutta (tämä on ensimmäinen aineopintojen harkkatyöni). 
 
-Koetin myös saada selkoa
+Koetin myös saada selkoa diskreetin fourier-muunnoksen käytännön toteuttamisesta ohjelmallisesti, alkaen siitä miten sen matematiikka toimisi tietokoneella. Lähteenä Signaalinkäsittelyn perusteet (Heikki Huttunen 2014),  https://urn.fi/URN:ISBN:978-952-15-3222-1 . Sain kiinni FFT:n periaatteesta, ja onnistuin laskemaan paperilla joitakin yksinkertaisia DFT-muunnoksia FFT-menetelmällä.
+
+
+
+
+
 
 Käytin tällä viikolla tähän aikaa 
 
---
+++ src/fft.py
@@ -0,0 +1,29 @@
+from cmath import exp
+from math import pi
+
+def fft(vektori):
+    pituus = len(vektori)
+    kerroin = exp(2 * pi * (0 + 1j) / pituus)
+    evens = []
+    odds = []
+    if pituus == 1:
+        return vektori
+    for i in range(0, pituus//2):
+        evens.append(vektori[i*2])
+        odds.append(vektori[i*2+1])
+    result_evens = fft(evens)
+    result_odds = fft(odds)
+    result = []
+    for k in range(pituus):
+        jj = k % pituus//2
+        #print(jj)
+        result.append(result_evens[jj] + (kerroin ** -k) * result_odds[jj])
+        print(result)
+    return result
+
+
+
+print(fft([0,1,2,3]))
+
+
+#w_N = exp(2 * pi * I / (double) N)
