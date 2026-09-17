4bbbab212dea291c0d3e4c6b153ab20fb13ecd02
Erkki Nurmi <183535537+eki-77@users.noreply.github.com>
2026-09-17 18:20:21 +0300

    Make class Analyser with init and fft methods

--- src/analyser.py
+++ src/analyser.py
@@ -1,18 +1,53 @@
 import wave
 import struct
+from cmath import exp
+from math import pi
 
-with wave.open("sample1.wav") as wav_sample:
-    metadata = wav_sample.getparams()
-    print(metadata)
-    frames = wav_sample.readframes(metadata.nframes)
-    print(len(frames))
-    format = "<" + "h" * (len(frames) // 2)
-    audio = list(struct.unpack(format, frames))
-    left_channel = audio[::metadata.nchannels]
-    #print(left_channel)
-    print(max(left_channel))
-    print(min(left_channel))
+class Analyser:
+    def __init__(self, sample):
+        # ladataan wav-tiedosto, jos kanavia on enemmäin kuin 1 otetaan vain 
+        # ensimmäinen eli vasen kanava.
+        with wave.open(sample) as wav_sample:
+            metadata = wav_sample.getparams()
+            if metadata.sampwidth != 2:
+                print("Virhe: ääninäytteen tulee olla 16-bittinen.")
+                quit()
+            #print(metadata)
+            frames = wav_sample.readframes(metadata.nframes)
+        format = "<" + "h" * (len(frames) // 2)
+        audio = list(struct.unpack(format, frames))
+        left_channel = audio[::metadata.nchannels]
+        #print(left_channel)
+        #print(max(left_channel))
+        #print(min(left_channel))
+        self.data = left_channel
+        self.samplerate = metadata.framerate
+    
+    def fft(self, vektori):
+        # vektorin pitää olla array, jonka pituus on kahden potenssi
+        pituus = len(vektori)
+        kerroin = exp(2 * pi * (0 + 1j) / pituus)
+        evens = []
+        odds = []
+        if pituus == 1:
+            return vektori
+        for i in range(0, pituus//2):
+            evens.append(vektori[i*2])
+            odds.append(vektori[i*2+1])
+        result_evens = self.fft(evens)
+        result_odds = self.fft(odds)
+        result = []
+        for k in range(pituus):
+            jj = k % (pituus // 2)
+            result.append(result_evens[jj] + (kerroin ** -k) * result_odds[jj])
+            #print(result)
+        # Lasketaan kompleksilukujen itseisarvot palautettavaksi
+        abs_result = [abs(x) for x in result]
+        return abs_result
+        
 
+testi = Analyser("sample1.wav")
+print(testi.fft([0,1,2,3]))
 
 
 
