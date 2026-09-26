054f5338dfe7c4fb94feb6e9a1011cc5ad6972c9
Erkki Nurmi <183535537+eki-77@users.noreply.github.com>
2026-09-26 11:26:13 +0300

    write first real unittest

--- src/tests/analyser_test.py
+++ src/tests/analyser_test.py
@@ -3,11 +3,12 @@ from analyser import Analyser
 
 class TestAnalyser(unittest.TestCase):
     def setUp(self):
-        print("Set up goes here")
+        self.testi = Analyser()
 
-    def test_hello_world(self):
-        self.assertEqual("Hello world", "Hello world")
-
-    #def test_valmistele_pidentaa_naytetta_oikein(self):
-
-    #    self.assertEqual()
+    def test_valmistele_pidentaa_naytetta_oikein(self):
+        pituus1 = len(self.testi.valmistele([1,2,3,4,5]))
+        pituus2 = len(self.testi.valmistele([1,2,3,4]))
+        pituus3 = len(self.testi.valmistele(([1]*1000000)))
+        self.assertEqual(pituus1, 8)
+        self.assertEqual(pituus2, 4)
+        self.assertEqual(pituus3, 1048576)
